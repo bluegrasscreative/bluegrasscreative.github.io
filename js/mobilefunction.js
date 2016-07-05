ead154e9cf4f96a7ca8c82e96efb6f4d64781e40
tombluegrasscoms <tom.small@bluegrasscoms.com>
2016-07-05 10:54:50 +0100

    *
    *

---
+++ js/mobilefunction.js
@@ -0,0 +1,5 @@
+if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    skrollr.init({
+        forceHeight: false
+    });
+}
