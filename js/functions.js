ed797fc750aeca73a7875b2998b5817cc3ee4948
tombluegrasscoms <tom.small@bluegrasscoms.com>
2016-07-06 15:44:46 +0100

    **
    **

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
+$(function() {
+  $('body').addClass('fadeinbody');
+});
+$(function() {
+  $('.navigation').addClass('bringinnav');
+});
 $(function(){
     $(".navigation, .behindov").click(function(){
         $(".navigation").toggleClass("navisopen");
     });
 });
-
 $(function(){
     $(".navigation, .behindov").click(function(){
         $("nav").toggleClass("opennav");
@@ -34,11 +39,38 @@ $(function(){
         $(".icon-wrap, .sidelogo").toggleClass("scaleup");
     });
 });
+$(function(){
+    $(".link").click(function(){
+        $(".behindov").toggleClass("appear");
+    });
+});
+$(function(){
+    $(".link").click(function(){
+        $(".navigation").toggleClass("navisopen");
+    });
+});
+
+$(function(){
+    $(".link").click(function(){
+        $("nav").toggleClass("opennav");
+    });
+});
+$(function(){
+    $(".link").click(function(){
+        $("body").toggleClass("fadeoutbody");
+    });
+});
+$(function(){
+    $(".link").click(function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+});
+
 $(".link").click(function () {
     var url = this.href;
     setTimeout(function () {
         location.href = url;
-    }, 1490);
+    }, 2500);
     return false;
 });
 $('.navigation, .behindov').click(function(){
